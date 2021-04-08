fdab8f067ceeb6acc131bf20ad5c423c0679f54a
unknown <ramgopal.kolla@archents.com>
2021-04-08 11:46:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngValidation';
+   angForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+   this.createForm();
+ }
+  createForm() {
+   this.angForm = this.fb.group({
+      name: ['', Validators.required ],
+      LastName:[ '', Validators.required],
+      Email:[ '', Validators.required],
+      address: ['', Validators.required ]
+   });
+ }
+login() {
+  console.log("login is called");
+}
+
 }
